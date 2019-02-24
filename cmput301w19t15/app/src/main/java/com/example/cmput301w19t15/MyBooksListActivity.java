0ed4355b284dfccd238d1da8f722bdc863d76b62
youruidong <43096991+youruidong@users.noreply.github.com>
2019-02-24 14:06:50 -0700

    acticity update

---
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/MyBooksListActivity.java
@@ -0,0 +1,13 @@
+package com.example.cmput301w19t15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyBooksListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_books_list);
+    }
+}
