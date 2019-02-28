d80308b84836eeeaf9e9e58c3223bc799ba51bb5
LAPTOP-CE96K5QQ\School <thomaspham345@gmail.com>
2019-02-28 16:45:22 -0700

    UIs

---
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/RequestedBookList.java
@@ -0,0 +1,13 @@
+package com.example.cmput301w19t15;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RequestedBookList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requested_book_list);
+    }
+}
