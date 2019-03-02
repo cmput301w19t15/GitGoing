5bd4201587479dfe0b23108075369c627c940971
proffesionalusrnm <mizcoolerthanu@gmail.com>
2019-03-01 18:48:38 -0700

    edited bookinfo class

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/BookInfo.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/BookInfo.java
@@ -3,6 +3,8 @@ package com.example.cmput301w19t15;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,16 +21,70 @@ public class BookInfo extends AppCompatActivity {
         final Book book = (Book) intent.getSerializableExtra("Book");
 
         //display book information as edit text
-        EditText title = (EditText)findViewById(R.id.editMyBookTitle);
-        EditText author = (EditText)findViewById(R.id.editMyBookAuthor);
-        EditText ISBN = (EditText)findViewById(R.id.editMyBookISBN);
+        final EditText title = (EditText)findViewById(R.id.editMyBookTitle);
+        final EditText author = (EditText)findViewById(R.id.editMyBookAuthor);
+        final EditText ISBN = (EditText)findViewById(R.id.editMyBookISBN);
 
         title.setText(book.getTitle());
         author.setText(book.getAuthor());
         ISBN.setText(book.getISBN());
 
-        //update any edits to firebase, so far no booklist in firebase to update
-        //also update mybooks list
+
+        //Scan book// to do
+
+        //update any edits to book *** editing should only be possible if user is owner ***
+        //also i could put this in a save button instead tbh
+        //then i wouldn't need the listeners...will consider in the future. I do need a scan book thing either way
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                book.setTitle(title.getText().toString());
+            }
+        });
+        author.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                book.setAuthor(author.getText().toString());
+            }
+        });
+        ISBN.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String temp = ISBN.getText().toString();
+                Integer isbn = Integer.valueOf(temp);
+                book.setISBN(isbn);
+            }
+        });
 
 
 
