ec9d3de0abecce33b1e48e88333440959023f316
StitchSizzler <eisha@ualberta.ca>
2019-03-02 21:12:23 -0700

    Revert "i dumb"

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/AddBookInfo.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/AddBookInfo.java
@@ -2,16 +2,12 @@
 package com.example.cmput301w19t15;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,15 +15,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddBookInfo extends AppCompatActivity {
+import static android.os.FileObserver.DELETE;
 
-    ImageView imageView;
+public class AddBookInfo extends AppCompatActivity {
 
     private EditText booktitle;
     private EditText author;
     private EditText isbn;
-
-    private Uri file;
 
     private String booktitleText;
     private String authorText;
@@ -38,23 +32,9 @@ public class AddBookInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book_info);
 
-
-        /*Button btnCamera = (Button) findViewById(R.id.takeBookPhoto);
-        ImageView imageView = (ImageView) findViewById(R.id.bookPhoto);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-            }
-        });*/
-
-
-
-        booktitle = (EditText) findViewById(R.id.editMyBookTitle);
-        author = (EditText) findViewById(R.id.editMyBookAuthor);
-        isbn = (EditText) findViewById(R.id.editMyBookISBN);
+        booktitle = (EditText) findViewById(R.id.booktitle);
+        author = (EditText) findViewById(R.id.author);
+        isbn = (EditText) findViewById(R.id.isbn);
 
         Button saveButton = findViewById(R.id.addBook);
 
@@ -98,30 +78,4 @@ public class AddBookInfo extends AppCompatActivity {
 
 
     }
-
-            /*photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                values.put(MediaStore.Images.Media.TITLE, "New Picture");
-                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
-                imageUri = getContentResolver().insert(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                /*Intent photoIntent = new Intent(AddBookInfo.this, BookPhoto.class);
-                photoIntent.putExtra("flag", "add");
-                startActivityForResult(photoIntent, 1000);
-            }
-        });*/
-
-
-
-
-    /*@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(resultCode, resultCode, data);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
-    }*/
 }
