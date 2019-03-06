50afdaa7c56ece89c44e36c79f764a59a76b4231
LAPTOP-CE96K5QQ\School <thomaspham345@gmail.com>
2019-03-06 13:03:03 -0700

    more minor changes
    - updated requestedbooklist.class

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/RequestedBookList.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/RequestedBookList.java
@@ -2,12 +2,18 @@ package com.example.cmput301w19t15;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class RequestedBookList extends AppCompatActivity {
+
+    private ListView acceptedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requested_book_list);
+
+        acceptedList = (ListView) findViewById(R.id.accepted);
+
     }
 }
