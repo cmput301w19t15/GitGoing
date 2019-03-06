9ab55f5fe30cb5fb840cba2f47565ff6906f6eab
LAPTOP-CE96K5QQ\School <thomaspham345@gmail.com>
2019-03-06 11:02:08 -0700

    created find books

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/FindBooks.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/FindBooks.java
@@ -1,8 +1,23 @@
 package com.example.cmput301w19t15;
 
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FindBooks extends AppCompatActivity {
+    private EditText bookSearch, filter2Add;
+    private Button addFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_books);
+
+        bookSearch = findViewById(R.id.search);
+        filter2Add = findViewById(R.id.filter);
+        addFilter = findViewById(R.id.addF);
+    }
 
 }
