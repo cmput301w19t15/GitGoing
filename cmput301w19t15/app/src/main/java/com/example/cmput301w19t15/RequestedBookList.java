477563b1f1ddf28128a9a789a0a753163e4b66d2
LAPTOP-CE96K5QQ\School <thomaspham345@gmail.com>
2019-03-11 13:52:10 -0600

    avadocs

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/RequestedBookList.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/RequestedBookList.java
@@ -1,4 +1,27 @@
+/*
+ * Class Name: RequestedBookList
+ *
+ * Version: 1.0
+ *
+ * Copyright 2019 TEAM GITGOING
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.example.cmput301w19t15;
+
+/**
+ * Adapter for book class
+ * @author Thomas, Josh
+ * @version 1.0
+ * @see BookAdapter
+ * @see User
+ * @since 1.0
+ */
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,9 +40,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+/**
+ * The type Requested book list.
+ */
 public class RequestedBookList extends AppCompatActivity implements BookAdapter.OnItemClickListener{
     private RequestedBookList activity = this;
-    private Button all, accepted;
+    //private Button all, accepted;
     private BookAdapter adapter;
     private ArrayList<Book> listOfBooks;
     private RecyclerView mRecyclerView;
@@ -45,6 +71,9 @@ public class RequestedBookList extends AppCompatActivity implements BookAdapter.
 
     }
 
+    /**
+     * Load books.
+     */
     public void loadBooks(){
         loadMyBookFromFireBase(new FindBooks.loadBookCallBack() {
             @Override
@@ -58,6 +87,11 @@ public class RequestedBookList extends AppCompatActivity implements BookAdapter.
         });
     }
 
+    /**
+     * Load my book from fire base.
+     *
+     * @param myCallback the my callback
+     */
     public void loadMyBookFromFireBase(final FindBooks.loadBookCallBack myCallback){
         DatabaseReference userReference = FirebaseDatabase.getInstance().getReference().child("books");
         userReference.addValueEventListener(new ValueEventListener() {
