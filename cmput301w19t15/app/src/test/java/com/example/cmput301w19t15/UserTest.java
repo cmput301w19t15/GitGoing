038009240e89f8d568a38ca73f1935e7fa51fd3d
LAPTOP-CE96K5QQ\School <thomaspham345@gmail.com>
2019-03-10 21:44:15 -0600

    more minor changes

--- cmput301w19t15/app/src/test/java/com/example/cmput301w19t15/UserTest.java
+++ cmput301w19t15/app/src/test/java/com/example/cmput301w19t15/UserTest.java
@@ -7,24 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class UserTest {
     @Test
     public void testUser() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
-        assertEquals(x.getUsername(),"user123");
+        User x = new User("first last", "example@email.com", "111-111-1111", "exampleID");
         assertEquals(x.getName(),"first last");
         assertEquals(x.getEmail(),"example@email.com");
         assertEquals(x.getPhone(),"111-111-1111");
     }
 
     @Test
-    public void testSetUsername() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
-        x.setUsername("Gradle");
-        assertEquals(x.getUsername(),"Gradle");
-        x.setUsername("Example2");
-        assertEquals(x.getUsername(),"Example2");
-    }
-    @Test
     public void testSetName() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
+        User x = new User("first last", "example@email.com", "111-111-1111", "exampleID");
         x.setName("Gradle Build");
         assertEquals(x.getName(),"Gradle Build");
         x.setName("Example Name");
@@ -32,7 +23,7 @@ public class UserTest {
     }
     @Test
     public void testSetEmail() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
+        User x = new User("first last", "example@email.com", "111-111-1111", "exampleID");
         x.setEmail("gradle@build.com");
         assertEquals(x.getEmail(),"gradle@build.com");
         x.setEmail("Uptown@funk.com");
@@ -40,7 +31,7 @@ public class UserTest {
     }
     @Test
     public void testSetPhone() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
+        User x = new User("first last", "example@email.com", "111-111-1111", "exampleID");
         x.setPhone("800-267-2001");
         assertEquals(x.getPhone(),"800-267-2001");
         x.setPhone("737-373-7373");
@@ -49,7 +40,7 @@ public class UserTest {
     @Test
     // Don't understand how photos work yet
     public void testSetRating() {
-        User x = new User("user123", "first last", "example@email.com", "111-111-1111", "exampleID");
+        User x = new User("first last", "example@email.com", "111-111-1111", "exampleID");
         x.setRating(5f);
         assertEquals(x.getRating(),(Float) 5f);
         x.setRating(4f);
