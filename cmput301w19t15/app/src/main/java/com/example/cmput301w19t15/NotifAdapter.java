043b251fae0e73d43369cfe6f53c14445e681bc7
Eisha Ahmed <e.ahmed5.ea@gmail.com>
2019-03-17 19:11:23 -0600

    not-push notifications done. Need to do push

--- cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/NotifAdapter.java
+++ cmput301w19t15/app/src/main/java/com/example/cmput301w19t15/NotifAdapter.java
@@ -1,13 +1,19 @@
 package com.example.cmput301w19t15;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -48,7 +54,7 @@ public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHol
     }
 
     @Override
-    public void onBindViewHolder (NotifAdapter.NotifViewHolder holder, int position) {
+    public void onBindViewHolder (final NotifAdapter.NotifViewHolder holder, int position) {
         Notification currentNotif = mNotifList.get(position);
 
         String notifType = "";
@@ -58,14 +64,68 @@ public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.NotifViewHol
         else if (currentNotif.getType().equals("accepted")) {
             notifType = "Your request has been accepted";
         }
-        String fromUser = FirebaseDatabase.getInstance().getReference().child("users")
-                .child(currentNotif.getNotifyFromID()).child("email").toString();
+        holder.mTextViewType.setText(notifType);
+
+
+
+        //get user email from ID
+        FirebaseDatabase.getInstance().getReference().child("users")
+                .child(currentNotif.getNotifyFromID()).child("email").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    if (dataSnapshot.getValue() != null) {
+                        try {
+                            String name = dataSnapshot.getValue().toString();
+                            holder.mTextViewUser.setText(name);
+                            //Log.e("TAG", "" + name[0]);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Log.e("TAG", " it's null.");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("onCancelled", " cancelled");
+            }
+        });
+
+
+        //get book title email from ID
+        FirebaseDatabase.getInstance().getReference().child("books")
+                .child(currentNotif.getBookID()).child("title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    if (dataSnapshot.getValue() != null) {
+                        try {
+                            String title = dataSnapshot.getValue().toString();
+                            holder.mTextViewBook.setText(title);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Log.e("TAG", " it's null.");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("onCancelled", " cancelled");
+            }
+        });
         String bookTitle = FirebaseDatabase.getInstance().getReference().child("books")
                 .child(currentNotif.getBookID()).child("title").toString();
 
-        holder.mTextViewType.setText(notifType);
-        holder.mTextViewUser.setText(fromUser);
-        holder.mTextViewBook.setText(bookTitle);
     }
 
     @Override
